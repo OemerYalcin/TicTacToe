4c08b59b6ca3670b1ea0e792d50c298c3c17abf8
OemerYalcin <79223320+OemerYalcin@users.noreply.github.com>
2021-04-24 01:01:23 +0200

    Begonnen mit der UI
    Begonnen die Serielle UI zu bauen

--- Basics/basics_test/basics_test.ino
+++ Basics/basics_test/basics_test.ino
@@ -1,9 +1,34 @@
+char Spielfeldstatus[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while(!Serial);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  UserIn();
+  delay(3000);
+}
 
+void UserIn(){
+  Serial.println(Spielfeldstatus[0]);
+  Serial.print("|");
+  Serial.print(Spielfeldstatus[1]);
+  Serial.print("|");
+  Serial.println(Spielfeldstatus[2]);
+  Serial.println("------------");
+  Serial.print(Spielfeldstatus[3]);
+  Serial.print("|");
+  Serial.print(Spielfeldstatus[4]);
+  Serial.print("|");
+  Serial.println(Spielfeldstatus[5]);
+  Serial.println("------------");
+  Serial.print(Spielfeldstatus[6]);
+  Serial.print("|");
+  Serial.print(Spielfeldstatus[7]);
+  Serial.print("|");
+  Serial.print(Spielfeldstatus[9]);
 }
